22fc9edb8b8a802ed302b05bca1301668e81b462
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-05-20 00:49:19 +0200

    add faker gem and seeds for database

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'devise'
 gem 'bootstrap-sass'
 
 gem 'haml'
+
+gem 'faker'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: "admin@example.com",
+							password: "password",
+							password_confirmation: "password",
+							admin: true)
+
+99.times do |n|
+	email = "example-#{n+1}@example.com"
+	password = "password"
+	User.create!(email: email,
+							password: password,
+							password_confirmation: password)
+end
+
+25.times do |n|
+	title = Faker::App.name
+	rate = rand(5) + 1
+	Game.create!(title: title,
+							rate: rate)
+end

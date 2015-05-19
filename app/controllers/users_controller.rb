198c6747f9465278e458a4860924c9c61739f8c9
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-05-20 01:56:56 +0200

    view improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:index]
 	before_action :admin_user, only: []
-	before_action :correct_user, only: [:show]
+	before_action :correct_user, only: []
 
   def show
     @user = User.find(params[:id])
